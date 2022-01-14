afb10b5d6e8463d394dd616b91be202ca7ceb3b8
Mehedi Hassan <howlader.mehedihassan@gmail.com>
2022-01-14 09:41:18 +0600

    feat: create components folder and add components

---
+++ src/components/list/list.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Classes from "./list.style.scss";
+
+export default function List() {
+    return (
+        <div>
+            <h1>List</h1>
+        </div>
+    );
+}
---
+++ src/components/search/search.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Classes from "./search.style.scss";
+
+export default function Search() {
+    return (
+        <div>
+            <h1>Search</h1>
+        </div>
+    );
+}
