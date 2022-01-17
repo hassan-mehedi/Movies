8457a2e5aa5e3a4bef07d10f8823e5883aee1927
Mehedi Hassan <howlader.mehedihassan@gmail.com>
2022-01-18 00:00:18 +0600

    feat: Add handler for controlling error message and data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "./components/search/search.component";
+import List from "./components/list/list.component";
 
 function App() {
-    const [search, setSearch] = useState("");
-    const API_KEY = process.env.REACT_APP_API_KEY;
-    const searchURL = `https://imdb-api.com/en/API/SearchMovie/${API_KEY}`;
+    const [listData, setListData] = useState([]);
+    const [error, setError] = useState(false);
 
+    // Firt time the list is empty, so we need to add some data
+    // This function is called only once to fill the list with batman movies
+    const startingMovieList = async () => {
+        const response = await fetch(`/suggestion/b/batman.json`, {
+            method: "GET",
+            mode: "cors",
+            headers: {
+                "Content-Type":
+                    "application/x-www-form-urlencoded; charset=UTF-8",
+            },
+        });
+        const data = await response.json();
+        setListData(data.d);
+    };
+
+    // This function is called everytime the user types something in the search bar
     const searchHandler = async (value) => {
-        setSearch(value);
-        console.log(searchURL);
-        const response = await fetch(`${searchURL}/${search}`);
+        console.log(value);
+        const response = await fetch(
+            `/suggestion/${value[0].toLowerCase()}/${value.toLowerCase()}.json`
+        );
         const data = await response.json();
         console.log(data);
-        console.log(search);
+        if (data.hasOwnProperty("d")) {
+            setError(false);
+            setListData(data.d);
+        } else {
+            setError(true);
+        }
     };
+
+    useEffect(() => {
+        startingMovieList();
+    }, []);
 
     return (
         <div className="App">
-            <Search handler={searchHandler} />
+            <Search handler={searchHandler} error={error} />
+            <List listData={listData} />
         </div>
     );
 }
